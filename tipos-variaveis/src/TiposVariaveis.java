a56ba580b4763e243e30bf4d42834d1ad0d44c57
adilsonsena <sejaassociado@gmail.com>
2023-01-08 02:19:41 -0300

    Deixando tudo comentado...

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -4,16 +4,16 @@ public class TiposVariaveis {
         // estudar a classe String que representa texto na aplicação
         // String meuNome = "Adilson Sena";
 
-        double salarioMinimo = 2500.33;
+        // double salarioMinimo = 2500.33;
 
-        short numeroCurto = 1;
-        int numeroNormal = numeroCurto;
-        short numeroCurto2 = (short) numeroNormal;
+        // short numeroCurto = 1;
+        // int numeroNormal = numeroCurto;
+        // short numeroCurto2 = (short) numeroNormal;
 
-        int numero = 5;
-        numero = 10;
-        System.out.print(numero);
+        // int numero = 5;
+        // numero = 10;
+        // System.out.print(numero);
 
-        final double VALOR_DE_PI = 3.14;
+        // final double VALOR_DE_PI = 3.14;
     }
 }
