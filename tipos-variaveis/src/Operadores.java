8c8215aa007c478e5837afe95ad2406662c26e03
adilsonsena <sejaassociado@gmail.com>
2023-01-09 05:59:46 -0300

    Boolean, if, & e ||.

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,9 +1,35 @@
 public class Operadores {
     public static void main(String[] args) {
+        boolean condicao1 = true;
+        boolean condicao2 = true;
+        if (condicao1 && (7 > 4)) {
+            System.out.println("As duas condições são verdadeiras");
+        }
+        if (condicao1 || condicao2) {
+            System.out.println("Uma das condições é verdadeira");
+        }
+        System.out.println("Fim");
+
+        // String nomeUm = "Adilson";
+        // String nomeDois = new String("Adilson");
+        // System.out.println(nomeUm.equals(nomeDois));
+
+        // int numero1 = 1;
+        // int numero2 = 2;
+        // boolean simNao = numero1 == numero2;
+        // if (numero1 < numero2) {
+        // System.out.println("a nossa condição é verdadeira");
+        // }
+        // System.out.println("numeroUm é igual a numeroDois? " + simNao);
+        // simNao = numero1 != numero2;
+        // System.out.println("numeroUm é diferente a numeroDois? " + simNao);
+        // simNao = numero1 > numero2;
+        // System.out.println("numeroUm é maior que numeroDois? " + simNao);
+
         // x repetição
-        int numero = 5;
-        numero = numero + 1;
-        System.out.println(numero);
+        // int numero = 5;
+        // numero = numero + 1;
+        // System.out.println(numero);
 
         // String nomeCompleto = "LINGUAGEM" + " JAVA";
         // System.out.println(nomeCompleto);
