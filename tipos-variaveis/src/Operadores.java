67e3298ba245d5fc19f5337526c43901ccca9b71
adilsonsena <sejaassociado@gmail.com>
2023-01-08 22:42:48 -0300

    repetição + 1

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,18 +1,23 @@
 public class Operadores {
     public static void main(String[] args) {
+        // x repetição
+        int numero = 5;
+        numero = numero + 1;
+        System.out.println(numero);
+
         // String nomeCompleto = "LINGUAGEM" + " JAVA";
         // System.out.println(nomeCompleto);
 
-        String concatenacao = "?";
-        concatenacao = 1 + 1 + 1 + "1";
-        System.out.println(concatenacao);
-        concatenacao = 1 + "1" + 1 + 1;
-        System.out.println(concatenacao);
-        concatenacao = 1 + "1" + 1 + "1";
-        System.out.println(concatenacao);
-        concatenacao = "1" + 1 + 1 + 1;
-        System.out.println(concatenacao);
-        concatenacao = "1" + (1 + 1 + 1);
+        // String concatenacao = "?";
+        // concatenacao = 1 + 1 + 1 + "1";
+        // System.out.println(concatenacao);
+        // concatenacao = 1 + "1" + 1 + 1;
+        // System.out.println(concatenacao);
+        // concatenacao = 1 + "1" + 1 + "1";
+        // System.out.println(concatenacao);
+        // concatenacao = "1" + 1 + 1 + 1;
+        // System.out.println(concatenacao);
+        // concatenacao = "1" + (1 + 1 + 1);
     }
 
 }
